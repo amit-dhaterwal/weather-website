eb9275b13dad8961fcc4ae566bf7cba63f0780a0
Amit Dhaterwal <amitdhaterwal12@gmail.com>
2019-12-11 18:15:17 +0530

    added some more info in about.hbs and added more forecast in forecastutils.js

--- src/utils/forecastUtils.js
+++ src/utils/forecastUtils.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback) => {
         } else if (body.error){
             callback("location not found",undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.' + ' The maximum temp today is ' + body.daily.data[0].temperatureMax + ' degrees and the minimum temp is ' + body.daily.data[0].temperatureMin + ' degrees')
         }
     })
 }
